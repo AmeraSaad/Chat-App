338bbd4f35a483053960b23bfaedd58421c431c4
Amera <amera@gmail.com>
2024-10-06 04:31:55 +0300

    add FrontEnd STARTER CODE

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,9 @@
+import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
-import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
-	return (
+  	return (
 		<div className='border-r border-slate-500 p-4 flex flex-col'>
 			<SearchInput />
 			<div className='divider px-3'></div>
